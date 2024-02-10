0f83bd9279dc05e251fad090522c6c56ada2990f
touredri <dcompte223@gmail.com>
2024-02-10 13:11:22 +0000

    setup

---
+++ src/main/java/com/yelensoft/artishop_backend/ArtishopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yelensoft.artishop_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtishopBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtishopBackendApplication.class, args);
+    }
+
+}
