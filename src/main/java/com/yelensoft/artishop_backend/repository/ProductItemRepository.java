879672da4c7fe43b0ce481300f98b10e369f372a
lonpo <dncper@gmail.com>
2024-04-23 00:25:12 +0200

    finish_without_testing

--- src/main/java/com/yelensoft/artishop_backend/repository/ProductItemRepository.java
+++ src/main/java/com/yelensoft/artishop_backend/repository/ProductItemRepository.java
@@ -1,11 +1,10 @@
 package com.yelensoft.artishop_backend.Repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.yelensoft.artishop_backend.model.ProductItem;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ProductItemRepository extends JpaRepository<ProductItem,Long> {
-List<ProductItem> findByCart_Id(Long id);
+    List<ProductItem> findByCart_Id(Long id);
 }
